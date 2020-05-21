08f65d61b9af91f2cf2ff0c1ff3150d6ea7bcc40
George Calin <george.calin@gmail.com>
2020-05-21 17:29:23 +0300

    Update main.cpp

--- 08_Threads_QSemaphore/main.cpp
+++ 08_Threads_QSemaphore/main.cpp
@@ -2,10 +2,42 @@
 #include <QDebug>
 #include <QThreadPool>
 #include <QSemaphore>
+#include "worker.h"
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
+    QStringList data;
+    for(int i = 0; i < 100; i++)
+    {
+        data.append(QString::number(i));
+    }
+
+    QThreadPool* pool = QThreadPool::globalInstance();
+    QSemaphore sema(100);
+
+    for (int i = 0;i < data.length();i++)
+    {
+        //Worker* worker = new Worker(&a,&data, &sema, i); Causes an error in windows 10
+        Worker* worker = new Worker(nullptr,&data, &sema, i);
+        worker->setAutoDelete(true);
+        pool->start(worker);
+    }
+    for (int i = 0;i < data.length();i++)
+    {
+       //Worker* worker = new Worker(&a,&data, &sema, i); Causes an error in windows 10
+        Worker* worker = new Worker(nullptr,&data, &sema, i);
+        worker->setAutoDelete(true);
+        pool->start(worker);
+    }
+    pool->waitForDone();
+
+    foreach(QString string, data)
+    {
+        qInfo() << string;
+    }
+
     return a.exec();
 }
+© 2020 GitHub, Inc.
