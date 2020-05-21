b8d12876d8c2bcb15ed4823c8d9251246a7c4556
George Calin <george.calin@gmail.com>
2020-05-21 17:30:47 +0300

    Update worker.h

--- 08_Threads_QSemaphore/worker.h
+++ 08_Threads_QSemaphore/worker.h
@@ -7,20 +7,21 @@
 #include <QRunnable>
 #include <QSemaphore>
 
-
-class worker : public QObject,public QRunnable
+class Worker : public QObject, public QRunnable
 {
     Q_OBJECT
 public:
-   explicit worker(QObject *parent = nullptr,QStringList* data=nullptr, QSemaphore* qSemaphore=nullptr,int position=-1);
+    explicit Worker(QObject *parent = nullptr, QStringList* data = nullptr, QSemaphore* sema = nullptr, int position = -1);
     void run();
 signals:
 
-private:
-        QStringList* qStringList;
-        QSemaphore* qSemaphore;
-        int position;
+public slots:
 
+private:
+    QStringList* data;
+    QSemaphore* sema;
+    int position;
 };
 
 #endif // WORKER_H
+© 2020 GitHub, Inc.
